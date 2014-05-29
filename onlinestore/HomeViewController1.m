97fdbec39d17ddd0f6dd827e18dbf84a76940780
rajenvision <envision@Envisions-Mac-mini.local>
2014-05-29 10:33:12 +0530

    Landing Screen Update

--- onlinestore/HomeViewController1.m
+++ onlinestore/HomeViewController1.m
@@ -70,88 +70,43 @@
     [tempLeftDic4 setObject:@"c.png" forKey:@"image"];
 
     [iconsLeft addObject:tempLeftDic4];
-    
-    NSMutableDictionary *tempLeftDic5=[[NSMutableDictionary alloc] init];
-    [tempLeftDic5 setObject:@"5" forKey:@"id"];
-    [tempLeftDic5 setObject:@"User Settings" forKey:@"name"];
-    [tempLeftDic5 setObject:@"150" forKey:@"height"];
-    [tempLeftDic5 setObject:@"#1996E6" forKey:@"color"];
-    [tempLeftDic5 setObject:@"j.png" forKey:@"image"];
-    [iconsLeft addObject:tempLeftDic5];
-    
-    
-    NSMutableDictionary *tempLeftDic6=[[NSMutableDictionary alloc] init];
-    [tempLeftDic6 setObject:@"6" forKey:@"id"];
-    [tempLeftDic6 setObject:@"Wish List" forKey:@"name"];
-    [tempLeftDic6 setObject:@"150" forKey:@"height"];
-    [tempLeftDic6 setObject:@"#FF7419" forKey:@"color"];
-    [tempLeftDic6 setObject:@"j.png" forKey:@"image"];
-    [iconsLeft addObject:tempLeftDic6];
 
-    
-      [iconsLeft addObject:tempLeftDic6];
-    
-  
     
     NSMutableArray *iconsRight=[[NSMutableArray alloc] init];
     
-    
     NSMutableDictionary *tempRightDic1=[[NSMutableDictionary alloc] init];
-    [tempRightDic1 setObject:@"7" forKey:@"id"];
-    [tempRightDic1 setObject:@"Browse & Shop" forKey:@"name"];
+    [tempRightDic1 setObject:@"6" forKey:@"id"];
+    [tempRightDic1 setObject:@"Wish List" forKey:@"name"];
     [tempRightDic1 setObject:@"150" forKey:@"height"];
-    [tempRightDic1 setObject:@"#F8BD19" forKey:@"color"];
-    [tempRightDic1 setObject:@"g.png" forKey:@"image"];
+    [tempRightDic1 setObject:@"#FF7419" forKey:@"color"];
+    [tempRightDic1 setObject:@"j.png" forKey:@"image"];
     [iconsRight addObject:tempRightDic1];
     
     NSMutableDictionary *tempRightDic2=[[NSMutableDictionary alloc] init];
-    [tempRightDic2 setObject:@"8" forKey:@"id"];
-    [tempRightDic2 setObject:@"Review Products" forKey:@"name"];
+    [tempRightDic2 setObject:@"6" forKey:@"id"];
+    [tempRightDic2 setObject:@"Browse & Shop" forKey:@"name"];
     [tempRightDic2 setObject:@"150" forKey:@"height"];
-    [tempRightDic2 setObject:@"#86B91C" forKey:@"color"];
-     [tempRightDic2 setObject:@"a.png" forKey:@"image"];
+    [tempRightDic2 setObject:@"#F8BD19" forKey:@"color"];
+    [tempRightDic2 setObject:@"g.png" forKey:@"image"];
     [iconsRight addObject:tempRightDic2];
     
     NSMutableDictionary *tempRightDic3=[[NSMutableDictionary alloc] init];
-    [tempRightDic3 setObject:@"9" forKey:@"id"];
-    [tempRightDic3 setObject:@"Shop Assistant" forKey:@"name"];
+    [tempRightDic3 setObject:@"7" forKey:@"id"];
+    [tempRightDic3 setObject:@"Review Products" forKey:@"name"];
     [tempRightDic3 setObject:@"150" forKey:@"height"];
-    [tempRightDic3 setObject:@"#7658F8" forKey:@"color"];
-    [tempRightDic3 setObject:@"f.png" forKey:@"image"];
-
+    [tempRightDic3 setObject:@"#86B91C" forKey:@"color"];
+     [tempRightDic3 setObject:@"a.png" forKey:@"image"];
     [iconsRight addObject:tempRightDic3];
     
     NSMutableDictionary *tempRightDic4=[[NSMutableDictionary alloc] init];
-    [tempRightDic4 setObject:@"10" forKey:@"id"];
-    [tempRightDic4 setObject:@"User Feedback" forKey:@"name"];
+    [tempRightDic4 setObject:@"8" forKey:@"id"];
+    [tempRightDic4 setObject:@"Shop Assistant" forKey:@"name"];
     [tempRightDic4 setObject:@"150" forKey:@"height"];
-    [tempRightDic4 setObject:@"#F8BD19" forKey:@"color"];
+    [tempRightDic4 setObject:@"#7658F8" forKey:@"color"];
+    [tempRightDic4 setObject:@"f.png" forKey:@"image"];
+
     [iconsRight addObject:tempRightDic4];
     
-    NSMutableDictionary *tempRightDic5=[[NSMutableDictionary alloc] init];
-    [tempRightDic5 setObject:@"11" forKey:@"id"];
-    [tempRightDic5 setObject:@"User Profile" forKey:@"name"];
-    [tempRightDic5 setObject:@"150" forKey:@"height"];
-    [tempRightDic5 setObject:@"#86B91C" forKey:@"color"];
-     [tempRightDic5 setObject:@"k.png" forKey:@"image"];
-    [iconsRight addObject:tempRightDic5];
-    
-    NSMutableDictionary *tempRightDic6=[[NSMutableDictionary alloc] init];
-    [tempRightDic6 setObject:@"12" forKey:@"id"];
-    [tempRightDic6 setObject:@"" forKey:@"name"];
-    [tempRightDic6 setObject:@"150" forKey:@"height"];
-    [tempRightDic6 setObject:@"#F8BD19" forKey:@"color"];
-    [tempRightDic6 setObject:@"k.png" forKey:@"image"];
-    [iconsRight addObject:tempRightDic6];
-    
-    NSMutableDictionary *tempRightDic7=[[NSMutableDictionary alloc] init];
-    [tempRightDic7 setObject:@"12" forKey:@"id"];
-    [tempRightDic7 setObject:@"" forKey:@"name"];
-    [tempRightDic7 setObject:@"150" forKey:@"height"];
-    [tempRightDic7 setObject:@"#F8BD19" forKey:@"color"];
-    [tempRightDic7 setObject:@"k.png" forKey:@"image"];
-    [iconsRight addObject:tempRightDic7];
-
     
     
     for (UIView *v in [tilesScrollView subviews]) {
@@ -236,18 +191,7 @@
     
     
     switch (index) {
-            //        case 1:{
-            //            CategoriesViewController *vc =[sb instantiateViewControllerWithIdentifier:@"CategoriesViewController"];
-            //            [self.navigationController pushViewController:vc animated:YES];
-            //            }
-            //            break;
-            //        case 2:{
-            //            BuildingMapViewController *vc =[sb instantiateViewControllerWithIdentifier:@"BuildingMapViewController"];
-            //            [self.navigationController pushViewController:vc animated:YES];
-            //        }
-            //            break;
-            
-        case 1:{
+                    case 1:{
             OffersAndDealsViewController *vc =[sb instantiateViewControllerWithIdentifier:@"OffersAndDealsViewController"];
             [self.navigationController pushViewController:vc animated:YES];
         }
@@ -271,60 +215,24 @@
         }
             break;
             
-            
+     
         case 5:{
-            UserSettingsViewController *vc =[sb instantiateViewControllerWithIdentifier:@"UserSettingsViewController"];
-            [self.navigationController pushViewController:vc animated:YES];
-        }
-            break;
-            
-        case 6:{
             UserProductSelectionListViewController *vc =[sb instantiateViewControllerWithIdentifier:@"UserProductSelectionListViewController"];
             [self.navigationController pushViewController:vc animated:YES];
         }
             break;
             
-        case 7:{
+        case 6:{
             BrowsAndShopViewController *vc =[sb instantiateViewControllerWithIdentifier:@"BrowsAndShopViewController"];
             [self.navigationController pushViewController:vc animated:YES];
         }
             break;
             
-        case 8:{
+        case 7:{
             ProductsViewController *vc =[sb instantiateViewControllerWithIdentifier:@"ProductsViewController"];
             [self.navigationController pushViewController:vc animated:YES];
         }
             break;
-        
-        
-        case 10:{
-            UserFeedBackViewController *vc =[sb instantiateViewControllerWithIdentifier:@"UserPreferenceViewController"];
-            [self.navigationController pushViewController:vc animated:YES];
-        }
-            break;
-            
-        case 11:{
-            UserProfileViewController *vc =[sb instantiateViewControllerWithIdentifier:@"UserProfileViewController"];
-            [self.navigationController pushViewController:vc animated:YES];
-        }
-            break;
-            
-//        case 12:{
-//            UserrSettingsViewController *vc =[sb instantiateViewControllerWithIdentifier:@"UserrSettingsViewController"];
-//            [self.navigationController pushViewController:vc animated:YES];
-//        }
-//            break;
-            
-            
-            
-            
-//        case 10:{
-//            UserProfileViewController *vc =[sb instantiateViewControllerWithIdentifier:@"UserProfileViewController"];
-//            [self.navigationController pushViewController:vc animated:YES];
-//        }
-//            break;
-            
-            
             
         default:{
             UIAlertView *alert=[[UIAlertView alloc] initWithTitle:@"Alert" message:@"Comming Soon.." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
@@ -332,9 +240,6 @@
         }
             break;
     }
-    
-    
-    
     
 }
 
